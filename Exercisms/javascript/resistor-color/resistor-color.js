64caa2cd7290b5174b60b2aaaeb0ae9444d3a44b
Jack Cullen <jpcullen20@gmail.com>
2020-10-26 10:23:17 -0600

    complete resistor challenge

--- Exercisms/javascript/resistor-color/resistor-color.js
+++ Exercisms/javascript/resistor-color/resistor-color.js
@@ -2,9 +2,9 @@
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+  const values = [...Array(10).keys()];
+  return values[COLORS.indexOf(color.toLowerCase())];
 };
-
-export const COLORS = undefined;
