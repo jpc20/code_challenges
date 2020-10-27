da7e374eab8078bfc4a181cd48f400c8db9bc708
Jack Cullen <jpcullen20@gmail.com>
2020-10-27 09:37:39 -0600

    Complete hello world in go

--- Exercisms/go/hello-world/hello_world_test.go
+++ Exercisms/go/hello-world/hello_world_test.go
@@ -2,11 +2,6 @@ package greeting
 
 import "testing"
 
-// Define a function named HelloWorld that takes no arguments,
-// and returns a string.
-// In other words, define a function with the following signature:
-// HelloWorld() string
-
 func TestHelloWorld(t *testing.T) {
 	expected := "Hello, World!"
 	if observed := HelloWorld(); observed != expected {
