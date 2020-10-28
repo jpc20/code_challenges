c91d49b6238a1a8eb19d56a1a95fc2fe679638d5
Jack Cullen <jpcullen20@gmail.com>
2020-10-28 10:58:39 -0600

    Working solution for raindrops challenge

--- Exercisms/python/raindrops/raindrops.py
+++ Exercisms/python/raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    response = ''
+    if number % 3 == 0:
+        response += 'Pling'
+    if number % 5 == 0:
+        response += 'Plang'
+    if number % 7 == 0:
+        response += 'Plong'
+    if (number % 3 != 0) & (number % 5 != 0) & (number % 7 != 0):
+        response = str(number)
+    return response
