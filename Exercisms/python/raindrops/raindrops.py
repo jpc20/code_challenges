092eeb1cb5c5aacf0cc32a7b236b7b1c65d4d5d8
Jack Cullen <jpcullen20@gmail.com>
2020-10-28 11:02:40 -0600

    Refactor solution

--- Exercisms/python/raindrops/raindrops.py
+++ Exercisms/python/raindrops/raindrops.py
@@ -1,11 +1,8 @@
 def convert(number):
     response = ''
-    if number % 3 == 0:
-        response += 'Pling'
-    if number % 5 == 0:
-        response += 'Plang'
-    if number % 7 == 0:
-        response += 'Plong'
+    if number % 3 == 0: response += 'Pling'
+    if number % 5 == 0: response += 'Plang'
+    if number % 7 == 0: response += 'Plong'
     if (number % 3 != 0) & (number % 5 != 0) & (number % 7 != 0):
         response = str(number)
     return response
