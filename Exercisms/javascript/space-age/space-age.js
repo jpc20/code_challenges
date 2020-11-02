f1e82408abb433f0344f9347ca9a549a4455dfad
Jack Cullen <jpcullen20@gmail.com>
2020-11-02 14:08:32 -0700

    Add calculations for all planets

--- Exercisms/javascript/space-age/space-age.js
+++ Exercisms/javascript/space-age/space-age.js
@@ -1,5 +1,11 @@
 export const age = (planet, secs) => {
-  let years = 0.00;
-  if (planet.toLowerCase() === 'earth') years = secs / 31557600.00
+  let years = secs / 31557600.00;
+  if (planet.toLowerCase() === 'mercury') years /= 0.2408467
+  if (planet.toLowerCase() === 'venus') years /= 0.61519726
+  if (planet.toLowerCase() === 'mars') years /= 1.8808158
+  if (planet.toLowerCase() === 'jupiter') years /= 11.862615
+  if (planet.toLowerCase() === 'saturn') years /= 29.447498
+  if (planet.toLowerCase() === 'uranus') years /= 84.016846
+  if (planet.toLowerCase() === 'neptune') years /= 164.79132
   return Math.round(years * 100) / 100
 };
