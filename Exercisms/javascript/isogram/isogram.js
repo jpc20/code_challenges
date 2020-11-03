dab6bbf6f82ca718182fd2788d7738aff20e238a
Jack Cullen <jpcullen20@gmail.com>
2020-11-03 13:50:14 -0700

    Working isogram solution

--- Exercisms/javascript/isogram/isogram.js
+++ Exercisms/javascript/isogram/isogram.js
@@ -1,8 +1,12 @@
-//
-// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isIsogram = () => {
-  throw new Error("Remove this statement and implement this function");
+export const isIsogram = (str) => {
+  let letters = {};
+  let isogram = true;
+  str.split('').forEach((char, i) => {
+    if (!letters[char.toLowerCase()] || char == ' ' || char == '-') {
+      letters[char.toLowerCase()] = true;
+    } else {
+      isogram = false;
+    }
+  });
+  return isogram;
 };
