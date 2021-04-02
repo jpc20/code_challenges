0999fb3e25729dbec8df2792f33cffd89c2f9062
Jack Cullen <jpcullen20@gmail.com>
2021-04-02 13:54:46 -0600

    Add and solve raindrops challenge

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn raindrops(n: u32) -> String {
+    let mut answer: String = "".to_owned();
+    if n % 3 == 0 { answer.push_str("Pling") };
+    if n % 5 == 0 { answer.push_str("Plang") };
+    if n % 7 == 0 { answer.push_str("Plong") };
+    if answer != "" { return answer } else { return n.to_string() };
+}
