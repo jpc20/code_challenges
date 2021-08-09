66988b8edf573f433498d2a1c94560c457ea8ad0
Jack Cullen <jpcullen20@gmail.com>
2021-08-09 09:25:13 -0600

    Test passing for single letters

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,3 +1,5 @@
+const compliments = { G: "C", C: "G", T: "A", A: "U" };
 export const toRna = (sequence) => {
-  if (sequence === '') return '';
+  if (sequence === "") return "";
+  return compliments[sequence]
 };
