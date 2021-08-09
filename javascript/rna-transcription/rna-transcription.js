0bc443ca755e0028d15087b32cf2be1b8b101f5a
Jack Cullen <jpcullen20@gmail.com>
2021-08-09 09:27:35 -0600

    Complete rna challenge

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,5 +1,8 @@
 const compliments = { G: "C", C: "G", T: "A", A: "U" };
 export const toRna = (sequence) => {
   if (sequence === "") return "";
-  return compliments[sequence]
+  return sequence
+    .split("")
+    .map((dna) => compliments[dna])
+    .join("");
 };
