7369dba14b7c32fd0205ed9538f4e0ea838bd9e6
Jack Cullen <jpcullen20@gmail.com>
2021-08-11 08:55:05 -0600

    Add default and shouting return values

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
-};
+    if (message === message.toUpperCase()) {
+      return "Whoa, chill out!"
+    } else {
+      return "Whatever."
+    }
+}
