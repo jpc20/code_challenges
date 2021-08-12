17b8caa9f069f6275acdab85e1bb54342fea51fa
Jack Cullen <jpcullen20@gmail.com>
2021-08-12 12:04:05 -0600

    Set up function and add base case

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const steps = (n) => {
+  const count = 0;
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+  return count;
 };
